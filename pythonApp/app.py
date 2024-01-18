ac02194fe92885a712fda1e658f5bc758fa6849f
Huy <maitran.mt02@gmail.com>
2024-01-18 10:50:47 -0600

    save

--- pythonApp/app.py
+++ pythonApp/app.py
@@ -20,35 +20,44 @@ def index():
 @app.route('/process_query', methods=['GET', 'POST'])
 def process_query():
     if request.method == 'POST':
-        raw_query = request.form['raw_query']
-        user_inputs.append(raw_query)
-        try:
-            # Using the MySQL connection
-            cursor = mysql.connection.cursor()
+        if 'submit-bttn' in request.form:
+            raw_query = request.form['raw_query']
+            user_inputs.append(raw_query)
+            try:
+                # Using the MySQL connection
+                cursor = mysql.connection.cursor()
+                
+                # Execute the raw query
+                cursor.execute(raw_query)
+                
+                # Fetch the results
+                header = (col[0] for col in cursor.description) # get column names
+                #header.insert(0, "ID") # add ID column
+                init_result = cursor.fetchall()
+                
+                
+                # Create a DataFrame from the results
+                result = pd.DataFrame(init_result, columns=header)
+                
+                # Convert DataFrame to HTML with Bootstrap styles
+                result_html = result.to_html(classes='table table-hover table-responsive', index= True, index_names=True)
+                
+                # Pass data and headings to the template
+                return render_template('index.html', table=result_html, user_inputs=user_inputs)
             
-            # Execute the raw query
-            cursor.execute(raw_query)
-            
-            # Fetch the results
-            header = (col[0] for col in cursor.description) # get column names
-            #header.insert(0, "ID") # add ID column
-            init_result = cursor.fetchall()
-            
-            
-            # Create a DataFrame from the results
-            result = pd.DataFrame(init_result, columns=header)
-            
-            # Convert DataFrame to HTML with Bootstrap styles
-            result_html = result.to_html(classes='table table-hover table-responsive', index= True, index_names=True)
-            
-            # Pass data and headings to the template
-            return render_template('index.html', table=result_html, user_inputs=user_inputs)
-        
-        except Exception as e:
-            error_message = f"Error executing query: {e}"
-            return render_template('index.html', table="", error_message=error_message, user_inputs=user_inputs)
-    else:
-        return render_template('index.html', user_inputs=user_inputs)
+            except Exception as e:
+                error_message = f"Error executing query: {e}"
+                return render_template('index.html', table="", error_message=error_message, user_inputs=user_inputs)
+        elif 'reset-bttn' in request.form:
+            user_inputs.clear()
+            return render_template('index.html', user_inputs=user_inputs)
+        else:
+            # for now, this is the only other button
+            return render_template('index.html', user_inputs=user_inputs)
 
+
+@app.route('/viz')
+def about():
+    return render_template('visualization.html')
 if __name__=='__main__':
     app.run(debug=True, use_reloader=True)
